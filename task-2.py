1aeb187ab51951b8d6c94aca0b3ad2307fbc4fc9
Nick Kutashenko <nick.kutashenko.space@gmail.com>
2022-04-20 21:35:26 +0200

    Add task 2

---
+++ task-2.py
@@ -0,0 +1,13 @@
+import sys
+
+a = float(input('a = '))
+c = float(input('c = '))
+
+n = a + 4 * c
+m = 2 * c + a
+if m == 0:
+    print('ERROR: divisor must not be zero')
+    sys.exit(-1)
+
+b = (n/m)
+print(b)
